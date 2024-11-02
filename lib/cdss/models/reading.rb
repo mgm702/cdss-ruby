e0553cc233eaec6f9f7a991b0fa2cdc5cf7cee0e
Matt Michnal <mattm3646@gmail.com>
2024-11-02 01:13:43 -0600

    fixing the parser and the models for better reuse

--- lib/cdss/models/reading.rb
+++ lib/cdss/models/reading.rb
@@ -1,33 +1,36 @@
 module Cdss
   module Models
-    class Station
+    class Reading
       ATTRIBUTES = %i[
         station_num
         abbrev
+        parameter
         usgs_site_id
-        name
-        agency
-        latitude
-        longitude
-        division
-        water_district
-        county
-        state
-        utm_x
-        utm_y
-        location_accuracy
-        start_date
-        end_date
+        meas_type
+        data_source
         modified
-        more_information
-        meas_unit
         metadata
-      ]
+        flags
+        meas_date
+        value
+        meas_unit
+        meas_value
+        meas_date_time
+        cal_year
+        cal_month_num
+        water_year
+        min_q_cfs
+        max_q_cfs
+        avg_q_cfs
+        total_q_af
+        meas_count
+      ].freeze
 
       attr_accessor(*ATTRIBUTES)
 
       def initialize(**attrs)
         attrs[:metadata] ||= {}
+        attrs[:flags] ||= {}
 
         ATTRIBUTES.each do |attr|
           instance_variable_set(:"@#{attr}", attrs[attr]) if attrs.key?(attr) && attrs[attr]
--- lib/cdss/parser.rb
+++ lib/cdss/parser.rb
@@ -2,33 +2,23 @@ module Cdss
   class Parser
     class << self
       def parse_station(response)
-        return nil unless response["ResultList"]&.first
+        return nil unless response['ResultList']&.first
 
-        data = response["ResultList"].first
+        data = response['ResultList'].first
         build_station(data)
       end
 
       def parse_stations(response)
-        return [] unless response["ResultList"]
+        return [] unless response['ResultList']
 
-        response["ResultList"].map { |data| build_station(data) }
+        response['ResultList'].map { |data| build_station(data) }
       end
 
-      def parse_readings(response)
-        return [] unless response["ResultList"]
+      def parse_readings(response, timescale:)
+        return [] unless response['ResultList']
 
-        response["ResultList"].map do |reading|
-          Cdss::Models::Reading.new(
-            timestamp: parse_timestamp(reading["measDateTime"] || reading["measDate"]),
-            value: reading["measValue"].to_f,
-            unit: reading["units"],
-            parameter: reading["parameter"],
-            quality: reading["flagA"],
-            metadata: {
-              stage: reading["stage"],
-              flag_b: reading["flagB"]
-            }
-          )
+        response['ResultList'].map do |reading|
+          build_reading(reading, timescale)
         end
       end
 
@@ -36,34 +26,102 @@ module Cdss
 
       def build_station(data)
         Cdss::Models::Station.new(
-          id: data["abbrev"],
-          name: data["stationName"],
-          agency: "DWR",
-          latitude: data["latitude"],
-          longitude: data["longitude"],
-          parameters: extract_parameters(data),
-          metadata: {
-            county: data["county"],
-            division: data["division"],
-            water_source: data["waterSource"],
-            usgs_id: data["usgsStationId"],
-            status: data["stationStatus"],
-            por_start: data["stationPorStart"],
-            por_end: data["stationPorEnd"]
-          }
+          station_num: data['stationNum'],
+          abbrev: data['abbrev'],
+          usgs_site_id: data['usgsSiteId'],
+          name: data['stationName'],
+          agency: data['dataSource'],
+          latitude: data['latitude'],
+          longitude: data['longitude'],
+          division: data['division'],
+          water_district: data['waterDistrict'],
+          county: data['county'],
+          state: data['state'],
+          utm_x: data['utmX'],
+          utm_y: data['utmY'],
+          location_accuracy: data['locationAccuracy'],
+          start_date: parse_timestamp(data['startDate']),
+          end_date: parse_timestamp(data['endDate']),
+          modified: parse_timestamp(data['modified']),
+          more_information: data['moreInformation'],
+          meas_unit: data['measUnit'],
+          metadata: {}
         )
       end
 
-      def parse_timestamp(datetime_str)
-        Time.parse(datetime_str)
-      rescue
-        nil
+      def build_reading(data, timescale)
+        base_params = {
+          station_num: data['stationNum'],
+          abbrev: data['abbrev'],
+          parameter: data['parameter'],
+          usgs_site_id: data['usgsSiteId'],
+          meas_type: data['measType'],
+          meas_unit: data['measUnit'],
+          meas_count: data['measCount']&.to_i,
+          meas_value: data['measValue']&.to_f,
+          meas_date: parse_timestamp(data['measDate']),
+          meas_date_time: parse_timestamp(data['measDateTime']),
+          data_source: data['dataSource'],
+          modified: parse_timestamp(data['modified']),
+          metadata: {}
+        }
+
+        case timescale
+        when :day
+          Cdss::Models::Reading.new(
+            **base_params,
+            value: data['value'].to_f,
+            flags: {
+              flagA: data['flagA'],
+              flagB: data['flagB'],
+              flagC: data['flagC'],
+              flagD: data['flagD']
+            },
+          )
+        when :month
+          Cdss::Models::Reading.new(
+            **base_params,
+            cal_year: data['calYear'].to_i,
+            cal_month_num: data['calMonNum'].to_i,
+            min_q_cfs: data['minQCfs'].to_f,
+            max_q_cfs: data['maxQCfs'].to_f,
+            avg_q_cfs: data['avgQCfs'].to_f,
+            total_q_af: data['totalQAf'].to_f,
+          )
+        when :year
+          Cdss::Models::Reading.new(
+            **base_params,
+            water_year: data['waterYear'].to_i,
+            min_q_cfs: data['minQCfs'].to_f,
+            max_q_cfs: data['maxQCfs'].to_f,
+            avg_q_cfs: data['avgQCfs'].to_f,
+            total_q_af: data['totalQAf'].to_f,
+          )
+        when :raw
+          Cdss::Models::Reading.new(
+            **base_params,
+            flags: {
+              flagA: data['flagA'],
+              flagB: data['flagB'],
+            },
+          )
+        when :hour
+          Cdss::Models::Reading.new(
+            **base_params,
+            flags: {
+              flagA: data['flagA'],
+              flagB: data['flagB'],
+            },
+          )
+        else
+          raise ArgumentError, "Invalid timescale: #{timescale}"
+        end
       end
 
-      def extract_parameters(data)
-        ["DISCHRG", "GAGE_HT"].select do |param|
-          data[param.downcase]
-        end
+      def parse_timestamp(datetime_str)
+        Time.parse(datetime_str) if datetime_str
+      rescue ArgumentError
+        nil
       end
     end
   end
