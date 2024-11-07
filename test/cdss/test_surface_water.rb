ff8edb1a2644406a31302af68d9e6596fc56ec7b
Matt Michnal <mattm3646@gmail.com>
2024-11-06 20:37:50 -0700

    adding pagination util function and fixing up tests

--- test/cdss/test_surface_water.rb
+++ test/cdss/test_surface_water.rb
@@ -15,8 +15,38 @@ class Cdss::TestSurfaceWater < Minitest::Test
     end
   end
 
-  def test_get_sw_ts
-    VCR.use_cassette('cdss_get_sw_ts') do
+  def test_get_sw_ts_day
+    VCR.use_cassette('cdss_get_sw_ts_day') do
+      readings = @client.get_sw_ts(
+        abbrev: 'PLACHECO',
+        start_date: Date.parse('2021-01-01'),
+        end_date: Date.parse('2021-12-31'),
+        timescale: 'day'
+      )
+      assert_kind_of Array, readings
+      refute_empty readings
+      reading = readings.first
+      assert_kind_of Cdss::Models::Reading, reading
+    end
+  end
+
+  def test_get_sw_ts_month
+    VCR.use_cassette('cdss_get_sw_ts_month') do
+      readings = @client.get_sw_ts(
+        abbrev: 'PLACHECO',
+        start_date: Date.parse('2021-01-01'),
+        end_date: Date.parse('2021-12-31'),
+        timescale: 'month'
+      )
+      assert_kind_of Array, readings
+      refute_empty readings
+      reading = readings.first
+      assert_kind_of Cdss::Models::Reading, reading
+    end
+  end
+
+  def test_get_sw_ts_year
+    VCR.use_cassette('cdss_get_sw_ts_year') do
       readings = @client.get_sw_ts(
         abbrev: 'PLACHECO',
         start_date: Date.parse('2021-01-01'),
