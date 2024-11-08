c4685e4d2dfe68d1cb32b5dee8640d57aac93fce
Matt Michnal <mattm3646@gmail.com>
2024-11-07 19:07:05 -0700

    fixing testing files for telemetry api calls

--- test/cdss/test_telemetry.rb
+++ test/cdss/test_telemetry.rb
@@ -15,14 +15,48 @@ class Cdss::TestTelemetry < Minitest::Test
     end
   end
 
-  def test_get_telemetry_ts
-    VCR.use_cassette('cdss_get_telemetry_ts') do
+  def test_get_telemetry_ts_day
+    VCR.use_cassette('cdss_get_telemetry_ts_day') do
       readings = @client.get_telemetry_ts(
         abbrev: 'PLACHECO',
         parameter: 'DISCHRG',
         start_date: Date.parse('2021-01-01'),
         end_date: Date.parse('2021-12-31'),
         timescale: 'day'
+      )
+      assert_kind_of Array, readings
+      refute_empty readings
+      reading = readings.first
+      assert_kind_of Cdss::Models::Reading, reading
+      assert_equal 'DISCHRG', reading.parameter
+    end
+  end
+
+  def test_get_telemetry_ts_hour
+    VCR.use_cassette('cdss_get_telemetry_ts_hour') do
+      readings = @client.get_telemetry_ts(
+        abbrev: 'PLACHECO',
+        parameter: 'DISCHRG',
+        start_date: Date.parse('2021-01-01'),
+        end_date: Date.parse('2021-12-31'),
+        timescale: 'hour'
+      )
+      assert_kind_of Array, readings
+      refute_empty readings
+      reading = readings.first
+      assert_kind_of Cdss::Models::Reading, reading
+      assert_equal 'DISCHRG', reading.parameter
+    end
+  end
+
+  def test_get_telemetry_ts_raw
+    VCR.use_cassette('cdss_get_telemetry_ts_raw') do
+      readings = @client.get_telemetry_ts(
+        abbrev: 'PLACHECO',
+        parameter: 'DISCHRG',
+        start_date: Date.parse('2021-01-01'),
+        end_date: Date.parse('2021-12-31'),
+        timescale: 'raw'
       )
       assert_kind_of Array, readings
       refute_empty readings
