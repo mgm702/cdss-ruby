c214f2da8fcb163e6bab2f7ca1d05cd79da790c5
Matt Michnal <mattm3646@gmail.com>
2024-11-19 18:58:49 -0700

    adding tests for water rights api calls to cdss

---
+++ test/cdss/test_water_rights.rb
@@ -0,0 +1,114 @@
+require 'test_helper'
+
+class Cdss::TestWaterRights < Minitest::Test
+  def setup
+    @client = Cdss::Client.new
+  end
+
+  def test_get_water_rights_net_amounts
+    VCR.use_cassette('cdss_get_water_rights_net_amounts') do
+      rights = @client.get_water_rights_net_amounts(county: 'Denver')
+      assert_kind_of Array, rights
+      refute_empty rights
+
+      right = rights.first
+      assert_kind_of Cdss::Models::WaterRight, right
+      assert_respond_to right, :wdid
+      assert_respond_to right, :county
+      assert_respond_to right, :division
+      assert_respond_to right, :water_district
+      assert_respond_to right, :modified
+    end
+  end
+
+  def test_get_water_rights_transactions
+    VCR.use_cassette('cdss_get_water_rights_transactions') do
+      rights = @client.get_water_rights_transactions(county: 'Denver')
+      assert_kind_of Array, rights
+      refute_empty rights
+
+      right = rights.first
+      assert_kind_of Cdss::Models::WaterRight, right
+      assert_respond_to right, :wdid
+      assert_respond_to right, :county
+      assert_respond_to right, :division
+      assert_respond_to right, :water_district
+      assert_respond_to right, :modified
+    end
+  end
+
+  def test_water_rights_aoi_search
+    VCR.use_cassette('cdss_get_water_rights_aoi_search') do
+      aoi = { latitude: 39.7392, longitude: -104.9903 }
+      rights = @client.get_water_rights_net_amounts(aoi: aoi, radius: 10)
+
+      assert_kind_of Array, rights
+      refute_empty rights
+
+      right = rights.first
+      assert_kind_of Cdss::Models::WaterRight, right
+      assert_respond_to right, :latitude
+      assert_respond_to right, :longitude
+    end
+  end
+
+  def test_water_rights_invalid_aoi
+    assert_raises(ArgumentError) do
+      @client.get_water_rights_net_amounts(aoi: { invalid: 'format' })
+    end
+
+    assert_raises(ArgumentError) do
+      @client.get_water_rights_transactions(aoi: [1])
+    end
+  end
+
+  def test_water_rights_pagination
+    VCR.use_cassette('cdss_get_water_rights_pagination') do
+      rights = @client.get_water_rights_net_amounts(division: 1)
+      assert_kind_of Array, rights
+      assert rights.length > 50000, "Should handle pagination for large result sets"
+    end
+  end
+
+  def test_water_rights_numeric_values
+    VCR.use_cassette('cdss_get_water_rights_numeric') do
+      rights = @client.get_water_rights_net_amounts(wdid: '0102834')
+      refute_empty rights
+
+      right = rights.first
+      assert_kind_of Integer, right.division unless right.division.nil?
+      assert_kind_of Integer, right.water_district unless right.water_district.nil?
+      assert_kind_of Float, right.latitude unless right.latitude.nil?
+      assert_kind_of Float, right.longitude unless right.longitude.nil?
+    end
+  end
+
+  def test_water_rights_date_formatting
+    VCR.use_cassette('cdss_get_water_rights_dates') do
+      rights = @client.get_water_rights_transactions(wdid: '0102834')
+      refute_empty rights
+
+      right = rights.first
+      assert_kind_of DateTime, right.modified unless right.modified.nil?
+    end
+  end
+
+  def test_get_water_rights_with_nil_parameters
+    VCR.use_cassette('cdss_get_water_rights_nil_params') do
+      rights = @client.get_water_rights_net_amounts
+      assert_kind_of Array, rights
+      refute_empty rights
+    end
+  end
+
+  def test_water_rights_array_format_aoi
+    VCR.use_cassette('cdss_get_water_rights_array_aoi') do
+      rights = @client.get_water_rights_transactions(
+        aoi: [-104.9903, 39.7392],
+        radius: 10
+      )
+      assert_kind_of Array, rights
+      refute_empty rights
+    end
+  end
+end
