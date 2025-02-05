0a3ca09a8ae5d2c8e0c31724c94c6bfa748497d2
Matt Michnal <mattm3646@gmail.com>
2025-02-04 20:15:48 -0700

    fixing up the analysis api calls for cdss gem

--- lib/cdss/parsers/analysis_parser.rb
+++ lib/cdss/parsers/analysis_parser.rb
@@ -1,110 +1,119 @@
+require_relative '../models/analysis'
+
 module Cdss
-  module Parsers
-    # Parses analysis-related data from CDSS API responses into appropriate model objects.
-    # Handles call analyses, source routes, and route analyses.
-    module AnalysisParser
-      extend BaseParser
+ module Parsers
+   module AnalysisParser
+     class << self
+       def parse_call_analyses(response, type:)
+         parse_collection(response) do |data|
+           # Cdss::Models::CallAnalysis.new(build_call_analysis_params(data, type))
+           params = build_call_analysis_params(data, type)
+           Cdss::Models::CallAnalysis.new(**params)
+         end
+       end
 
-      class << self
-        # Parses call analysis data from API response.
-        #
-        # @param response [Hash] The API response containing call analysis data
-        # @param type [Symbol] The type of analysis (:wdid or :gnis)
-        # @return [Array<Cdss::Models::CallAnalysis>] Array of call analysis objects
-        def parse_call_analyses(response, type:)
-          parse_collection(response) { |data| build_analysis(Cdss::Models::CallAnalysis, data, type) }
-        end
+       def parse_source_routes(response)
+         parse_collection(response) do |data|
+           params = build_source_route_params(data)
+           Cdss::Models::SourceRoute.new(**params)
+         end
+       end
 
-        # Parses source route data from API response.
-        #
-        # @param response [Hash] The API response containing source route data
-        # @return [Array<Cdss::Models::SourceRoute>] Array of source route objects
-        def parse_source_routes(response)
-          parse_collection(response) { |data| build_analysis(Cdss::Models::SourceRoute, data) }
-        end
+       def parse_route_analyses(response)
+         parse_collection(response) do |data|
+           Cdss::Models::RouteAnalysis.new(build_route_analysis_params(data))
+         end
+       end
 
-        # Parses route analysis data from API response.
-        #
-        # @param response [Hash] The API response containing route analysis data
-        # @return [Array<Cdss::Models::RouteAnalysis>] Array of route analysis objects
-        def parse_route_analyses(response)
-          parse_collection(response) { |data| build_analysis(Cdss::Models::RouteAnalysis, data) }
-        end
+       private
 
-        private
+       def parse_collection(response)
+         return [] unless response && response['ResultList']
+         response['ResultList'].map { |data| yield(data) }
+       end
 
-        def build_analysis(klass, data, type = nil)
-          params = case klass
-          when Cdss::Models::CallAnalysis
-            build_call_analysis_params(data, type)
-          when Cdss::Models::SourceRoute
-            build_source_route_params(data)
-          when Cdss::Models::RouteAnalysis
-            build_route_analysis_params(data)
-          end
+       def parse_timestamp(value)
+         return nil if value.nil? || value.to_s.empty?
+         DateTime.parse(value)
+       rescue
+         nil
+       end
 
-          klass.new(**params)
-        end
+       def safe_float(value)
+         Float(value) rescue nil
+       end
 
-        def build_call_analysis_params(data, type)
-          params = {
-            analysis_date: parse_timestamp(data['analysisDate']),
-            admin_number: safe_float(data['adminNo']),
-            percent_time_out_of_priority: safe_float(data['percentTimeOutOfPriority']),
-            downstream_call_wdid: data['downstreamCallWdid'],
-            downstream_call_right: data['downstreamCallRight'],
-            downstream_call_stream_mile: safe_float(data['downstreamCallStreamMile']),
-            downstream_call_admin_number: safe_float(data['downstreamCallAdminNo']),
-            downstream_call_decreed_amount: safe_float(data['downstreamCallDecreedAmount']),
-            downstream_call_decreed_unit: data['downstreamCallDecreedUnit'],
-            downstream_call_appropriation_date: parse_timestamp(data['downstreamCallAppropriationDate']),
-            downstream_call_status: data['downstreamCallStatus'],
-            modified: parse_timestamp(data['modified']),
-            metadata: {}
-          }
+       def safe_integer(value)
+         Integer(value) rescue nil
+       end
 
-          case type
-          when :wdid
-            params[:wdid] = data['wdid']
-          when :gnis
-            params[:gnis_id] = data['gnisId']
-            params[:stream_mile] = safe_float(data['streamMile'])
-          end
+       def build_call_analysis_params(data, type)
+         return {} unless data
 
-          params
-        end
+         {
+           analysis_date: parse_timestamp(data['analysisDate']),
+           wdid: data['analysisWdid'],
+           admin_number: safe_float(data['analysisWrAdminNo']),
+           percent_time_out_of_priority: safe_float(data['analysisOutOfPriorityPercentOfDay']),
+           downstream_call_wdid: data['locationWdid'],
+           downstream_call_right: data['priorityStructure'],
+           downstream_call_stream_mile: safe_float(data['locationWdidStreamMile']),
+           downstream_call_admin_number: safe_float(data['priorityAdminNo']),
+           downstream_call_decreed_amount: nil,
+           downstream_call_decreed_unit: nil,
+           downstream_call_appropriation_date: parse_timestamp(data['priorityDate']),
+           downstream_call_status: data['callType'],
+           modified: parse_timestamp(data['dateTimeSet']),
+           metadata: {
+             division: safe_integer(data['division']),
+             date_time_released: parse_timestamp(data['dateTimeReleased']),
+             water_source_name: data['waterSourceName'],
+             location_structure: data['locationStructure'],
+             priority_order_no: safe_integer(data['priorityOrderNo']),
+             priority_no: data['priorityNo'],
+             bounding_wdid: data['boundingWdid'],
+             bounding_structure_name: data['boundingStructureName'],
+             set_comments: data['setComments'],
+             release_comment: data['releaseComment']
+           }
+         }.compact
+       end
 
-        def build_source_route_params(data)
-          {
-            gnis_id: data['gnisId'],
-            gnis_name: data['gnisName'],
-            stream_name: data['streamName'],
-            division: safe_integer(data['division']),
-            water_district: safe_integer(data['waterDistrict']),
-            county: data['county'],
-            start_mile: safe_float(data['startMile']),
-            end_mile: safe_float(data['endMile']),
-            total_length: safe_float(data['totalLength']),
-            modified: parse_timestamp(data['modified']),
-            metadata: {}
-          }
-        end
+       def build_source_route_params(data)
+         return {} unless data
 
-        def build_route_analysis_params(data)
-          {
-            wdid: data['wdid'],
-            structure_name: data['structureName'],
-            stream_mile: safe_float(data['streamMile']),
-            structure_type: data['structureType'],
-            decreed_amount: safe_float(data['decreedAmount']),
-            decreed_unit: data['decreedUnit'],
-            appropriation_date: parse_timestamp(data['appropriationDate']),
-            admin_number: safe_float(data['adminNo']),
-            modified: parse_timestamp(data['modified']),
-            metadata: {}
-          }
-        end
-      end
-    end
-  end
+         {
+           gnis_id: data['gnisId'],
+           gnis_name: data['gnisName'],
+           stream_name: data['streamName'],
+           division: safe_integer(data['division']),
+           water_district: safe_integer(data['waterDistrict']),
+           county: data['county'],
+           start_mile: safe_float(data['startMile']),
+           end_mile: safe_float(data['endMile']),
+           total_length: safe_float(data['totalLength']),
+           modified: parse_timestamp(data['modified']),
+           metadata: {}
+         }.compact
+       end
+
+       def build_route_analysis_params(data)
+         return {} unless data
+
+         {
+           wdid: data['wdid'],
+           structure_name: data['structureName'],
+           stream_mile: safe_float(data['streamMile']),
+           structure_type: data['structureType'],
+           decreed_amount: safe_float(data['decreedAmount']),
+           decreed_unit: data['decreedUnit'],
+           appropriation_date: parse_timestamp(data['appropriationDate']),
+           admin_number: safe_float(data['adminNo']),
+           modified: parse_timestamp(data['modified']),
+           metadata: {}
+         }.compact
+       end
+     end
+   end
+ end
 end
